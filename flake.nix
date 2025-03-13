bb1b193e564af51c409ffdf4973a27647d9095a8
TheWanderingCrow <contact@wanderingcrow.net>
2025-03-13 13:22:01 -0400

    php is not stupid

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Alchemist flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "alchemist";
+          packages = with pkgs; [
+            php83
+            php83Packages.composer
+          ];
+        };
+      };
+    };
+}
+
